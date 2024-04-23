7b6dd77c3310e356bc7a207c5212d8dfc499e52f
Jiho <jiho.lee@ufl.edu>
2024-04-23 11:54:46 -0400

    first commit

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Authentic Products Guaranteed",
+    detail:
+      "Our SS4U authentication team works hard to guarantee that every single product sold by us are real, authentic supplies from our customers' favorite brands. ",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Free Returns and Exchanges",
+    detail:
+      "If you ever need to return a product or want to exchange for a different unit due to a defect or sizing issue, we are happy to help, as long as it is within 30 days of purchase.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Price Matching",
+    detail:
+      "We guarantee that SS4U's products are offered at the best prices across the market. If you find an authentic product sold at a lower price at a different retailer, we will price match to ensure that you get the best deal pssible.",
+  },
+];
+export default data;
